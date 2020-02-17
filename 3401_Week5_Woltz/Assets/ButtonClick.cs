74efd7cd3a384ce5ee4a62b275460ffd47100e84
DanaWoltz <50628247+DanaWoltz@users.noreply.github.com>
2020-02-17 15:23:23 -0500

    Homework5
    UI elements

---
+++ 3401_Week5_Woltz/Assets/ButtonClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+
+    public AudioSource buttonAudio;
+
+
+    public void OnButtonClick()
+    {
+        buttonAudio.Play();
+    }
+    public void StopStartAudio()
+    {
+        if (buttonAudio.isPlaying)
+        {
+            buttonAudio.Stop();
+        }
+        else
+        {
+            buttonAudio.Play();
+        }
+    }
+}
